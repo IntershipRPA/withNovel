03cd212d855162becdc99807fe30d220b1681ab3
tjrgh <tjrgh88@gmail.com>
2023-10-12 17:55:53 +0900

    코드 통합

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Editor from "./components/Editor.vue";
+</script>
+
+<template>
+  <div class="mt-8">
+    <Editor completion-api="http://localhost:3000/api/generate" />
+  </div>
+</template>
+
+<style scoped></style>
